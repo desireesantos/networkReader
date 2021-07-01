a34368f9988e45aa0b89175909ad7d7279f105e5
Desiree Santos <desisant@gmail.com>
2021-07-01 01:37:37 -0300

    Add parse script

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # NetworkReader
 Parse packages in real time from network using Tshark[Wireshark]
 
-###Requirement
+##Requirement
  * Install [Wireshard](https://www.wireshark.org/#download) and don't forget to enable the tshark.
 
  Tshart is a command-line network protocol analyzer.
---
+++ script/analiseNetworkData.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+index=0
+
+# Create directories to store files
+mkdir pcapFiles
+mkdir svcFiles
+
+# Filter and save UDP and COAP package from network
+tshark -i 1  -f "dst port 5683 and ip dst 192.168.43.73" -b filesize:1 -a files:2 -w ./pcapFiles/network_data.pcap
+
+# Convert .pcap to .csv file
+for filename in ./pcapFiles/*.pcap; do
+    tshark -r "$filename"  -T fields -E separator=, -e ip.src -e ip.dst -e _ws.col.Protocol -e coap.payload_length  -e _ws.col.Info >  "./svcFiles/extracted_${index}.svc"
+    index=$(( index + 1 ))
+done
