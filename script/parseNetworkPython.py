29c9e4f7df919baf0aece1c2ed150f6c7f2b1375
Desiree Santos13 <dsantos13@lenovo.com>
2021-08-19 00:43:25 -0300

    Capturing package size for coap and mqtt for all quality of service

--- script/parseNetworkPython.py
+++ script/parseNetworkPython.py
@@ -1,11 +1,17 @@
 import pyshark
 from influxdb import InfluxDBClient
+from statistics import mean
 
 host = 'localhost'
 port = 8086
-json_body = []
+result = []
 networkInterface = "en0"
-filter_CoAP_MQTT = "dst port 1883 or dst port 5683"
+filter_CoAP_MQTT = "(dst port 5683 or src port 5683) or (dst port 1883 or src port 1883)"
+
+lenght = 0
+small_size = 100
+large_size = 500
+package_lost_limit = 2
 
 client = InfluxDBClient(host, port)
 client.switch_database('middleware')
@@ -14,86 +20,34 @@ captureFullPackage = pyshark.LiveCapture(interface=networkInterface, bpf_filter=
 captureSummary = pyshark.LiveCapture(interface=networkInterface, bpf_filter=filter_CoAP_MQTT, only_summaries=True)
 
 print("listening on %s" % networkInterface)
+class Package(object):  
+    def __init__(self,size):
+        self.size=size
 
-def buildMQTTJson(captureFullPackage, captureSummary):
-        transport_layer = captureFullPackage.transport_layer    # protocol transport_layer
-        src_addr = captureFullPackage.ip.src                    # source address
-        dst_addr = captureFullPackage.ip.dst                    # destination address
-        protocol = captureSummary.protocol                      # protocol type
-        package_time = captureSummary.time                      # package time
-        status = captureSummary.info
-    
-        payload_size = captureFullPackage.mqtt.len              # payload size
-        mid = captureFullPackage.mqtt.msgid                     # Messager identifier
-        method_type = captureFullPackage.mqtt.msgtype           # Message type
-        uri_path = captureFullPackage.mqtt.topic                # topic name
-        qos = captureFullPackage.mqtt.sub_qos                   # quality of Service
+def getPackageSize(package):
+    if (package.size):
+        return package.size
+    else:
+        return 0    
 
-        json_body = [
-            {
-                "measurement": "middleware",
-                "fields": {
-                    "transport_layer": transport_layer,
-                    "protocol": protocol,
-                    "payload_size": payload_size,
-                    "src_addr": src_addr,
-                    "dst_addr": dst_addr,
-                    "status": status,
-                    "mid": mid,
-                    "method_type": method_type,
-                    "uri_path": uri_path,
-                    "package_time": package_time,
-                    "qos": qos
-                }
-            }
-        ]
-        return json_body
+def calculate_package_size_average(packages):
+    return mean( list(map(getPackageSize, packages)) )
 
-def buildCoAPJson(captureFullPackage,captureSummary):
-            transport_layer = captureFullPackage.transport_layer    # protocol transport_layer
-            payload_size = captureFullPackage.coap.payload_length   # payload size
-            src_addr = captureFullPackage.ip.src                    # source address
-            dst_addr = captureFullPackage.ip.dst                    # destination address
-            protocol = captureSummary.protocol                      # protocol 
-            package_time = captureSummary.time                      # package time
-            
-            package_info = captureSummary.info.split(",")           # wireshark summary info
-            status = package_info[0].strip()
-            mid = package_info[1].strip()
-            method_type = package_info[2].strip()
-            uri_path = package_info[3].strip()
-
-            json_body = [
-                {
-                    "measurement": "middleware",
-                    "fields": {
-                        "transport_layer": transport_layer,
-                        "protocol": protocol,
-                        "payload_size": payload_size,
-                        "src_addr": src_addr,
-                        "dst_addr": dst_addr,
-                        "status": status,
-                        "mid": mid,
-                        "method_type": method_type,
-                        "uri_path": uri_path,
-                        "package_time": package_time
-                    }
-                }
-            ]
-            return json_body
-
+def identify_package_size(package_size):
+    if (package_size <= small_size): return 'SMALL'
+    if (package_size > small_size and package_size <= large_size): return 'MEDIUM'
+    if (package_size > large_size): return 'LARGE'    
 
 for captureFullPackage,captureSummary in zip(captureFullPackage.sniff_continuously(), captureSummary.sniff_continuously()):
+    lenght = captureSummary.length
 
-    try:
-        if captureSummary.protocol == 'CoAP':
-            json_body =  buildCoAPJson(captureFullPackage,captureSummary)
-        else:
-            json_body =  buildMQTTJson(captureFullPackage,captureSummary)      
-        
-        client.write_points(json_body) 
-        # print("Write points: {0}".format(json_body))
-        
-    except AttributeError as e:
-        pass
-    print (" ")
+    if( hasattr(captureFullPackage,'mqtt')):
+        lenght = captureFullPackage.mqtt.len
+  
+    if(len(result) < 4):
+      result.append( Package( int(lenght) ))
+    else:
+       package_size_avr = calculate_package_size_average(result)
+       print(identify_package_size(package_size_avr))
+       result = []
+       result.append( Package(int(lenght)) )
